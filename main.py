ad91256b56eed61bfa2d7d3fb1ad6bd31745129f
linhai <akarin_linh@outlook.com>
2023-11-23 14:31:32 +0800

    update: 所有需要接口的实现

--- main.py
+++ main.py
@@ -1,38 +1,75 @@
-from typing import List, Union
+from typing import Any, Dict, List, Union
 from fastapi import FastAPI
 from myquant.service import MyQuantApi
 from pydantic import BaseModel
 from util.converter import parse_myquant_vt_symbol
 from util.logger import log
+import uvicorn
 
 app = FastAPI()
 api = MyQuantApi()
 
+def verify_symbols(vt_symbols) -> Union[Dict[str, Any], None]:
+    # type: (str) -> Dict[str, Any]
+    for vt_symbol in vt_symbols.split(','):
+        vt_symbol = parse_myquant_vt_symbol(vt_symbol)        
+        if vt_symbol == None:
+            return {"code": 400, "data": None, "msg": "不合法的合约代码"}
+    return None
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
 
+@app.get("/csymbol")
+def csymbol(csymbol: str):
+    return api.get_continuous_contracts(csymbol)
+
+@app.get("/current")
+def current(vt_symbols: str, fields: str = ''):
+    return api.current(vt_symbols, fields)
+
 @app.get("/parse/{vt_symbol}")
 def read_root(vt_symbol: str):
     vt_symbol = parse_myquant_vt_symbol(vt_symbol)
     return {"Hello": vt_symbol}
 
+@app.get("/session")
+def trade_session(vt_symbol: str):
+    ok = verify_symbols(vt_symbol)
+    if ok:
+        return ok
+    log.info('rua')
+    result = api.get_trading_session(vt_symbol)
+    return {"code": 200, "data": result, "msg": "查询成功"}
+
+@app.get("/history_symbol")
+def symbols(symbol: str, start: str, end: str):
+    symbols_info = api.get_hisotry_symbol(symbol, start, end)
+    return symbols_info
+
 @app.get("/symbols")
-def symbols():
-    symbols_info = api.get_symbol_infos()
+def symbols(origin: int, sub_origin: int):
+    symbols_info = api.get_symbol_infos(origin, sub_origin)
     return symbols_info
 
 @app.get("/history")
 def history(vt_symbols: str, start: str, end: str, interval: str = '1d', adjust: int = 1, fill_missing: str = 'Last'):
-    for vt_symbol in vt_symbols.split(','):
-        vt_symbol = parse_myquant_vt_symbol(vt_symbol)        
-        if vt_symbol == None:
-            return {"code": 400, "data": None, "msg": "不合法的合约代码"}
-
+    ok = verify_symbols(vt_symbols)
+    if ok:
+        return ok
     result = api.history(vt_symbols, start, end, interval, adjust, fill_missing)
     return {"code": 200, "data": result, "msg": "查询成功"}
+
+@app.get("/subscribe")
+def subscribe(vt_symbols, interval, count = 2):
+    # type: (str, str, int) -> None
+    ok = verify_symbols(vt_symbols)
+    if ok:
+        return ok
+    result = api.subscribe(vt_symbols, interval, count)
+    return {"code": 200, "data": result, "msg": "订阅成功"}
 
 
 @app.get("/items/{item_id}")
@@ -42,3 +79,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- util/converter.py
+++ util/converter.py
@@ -1,4 +1,6 @@
-from typing import Union
+import re
+from typing import List, Tuple, Union
+from model.enum import Exchange
 
 from util.logger import log
 
@@ -8,3 +10,60 @@ def parse_myquant_vt_symbol(standard_vt_symbol: str) -> Union[str, None]:
         log.error(f'传入的合约代码不合法, {standard_vt_symbol}')
         return None
     return f'{symbol_segment[1]}.{symbol_segment[0]}'
+
+def vt_to_gm_symbol(vt_symbol: str) -> Union[str, None]:
+    symbol_segment: List[str] = []
+    
+    symbol_segment = vt_symbol.split('.')
+    if len(symbol_segment) < 2:
+        log.error(f'传入的合约代码不合法, {vt_symbol}')
+        return None
+
+    if '9999' in symbol_segment[0]:
+        symbol_segment[0] = symbol_segment[0][:-4]
+    
+    if Exchange(symbol_segment[1]) == Exchange.CZCE:
+        code, exchange = extract_future_vt_symbol(vt_symbol)
+        symbol_segment[0] = code + symbol_segment[0][len(code) + 1:]
+
+    if symbol_segment[1] == 'SSE':
+        symbol_segment[1] = 'SHSE'
+    
+    return f'{symbol_segment[1]}.{symbol_segment[0]}'
+
+def extract_future_vt_symbol(vt_symbol: str) -> Tuple[str, Exchange]:
+    symbol, exchange = extract_vt_symbol(vt_symbol)
+
+    if exchange == Exchange.CFFEX and 'LX' in symbol:
+        code = symbol.split('LX')[0]
+    else:
+        start, end = re.search('\D\d', symbol).span()
+        code = symbol[:(end - 1)]
+    return code, exchange
+
+def extract_vt_symbol(vt_symbol: str) -> Tuple[str, Exchange]:
+    """
+    :return: (symbol, exchange)
+    """
+    if vt_symbol.startswith(".") and vt_symbol.count(".") == 2:
+        _, symbol, exchange_str = vt_symbol.split(".")
+        return "." + symbol, Exchange(exchange_str)
+    else:
+        symbol, exchange_str = vt_symbol.split(".")
+        return symbol, Exchange(exchange_str)
+    
+def extract_symbol(vt_symbols: str) -> str:
+    param_symbols: str = ""
+
+    for vt_symbol in (vt_symbols + ',').split(','):
+        if vt_symbol == "":
+            continue
+
+        vt_symbol = vt_to_gm_symbol(vt_symbol)
+        if not vt_symbol:
+            continue
+        param_symbols += vt_symbol + ','
+
+    param_symbols = param_symbols[:-1]
+
+    return param_symbols
