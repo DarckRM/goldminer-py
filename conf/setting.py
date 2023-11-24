8662bf11a96555af7ffe4c7d6b024a6869866643
linhai <akarin_linh@outlook.com>
2023-11-24 17:16:12 +0800

    update: 更新一些基础配置

--- conf/setting.py
+++ conf/setting.py
@@ -5,7 +5,15 @@ from conf.setting_dev import SETTINGS_DEV
 
 SETTINGS: Dict[str, Any] = {
     "setting.env": "dev",
-    "token": ""
+    "token": "",
+
+    "serv_addr": "",
+
+    # 开发
+    "redis_host": "127.0.0.1",
+    "redis_pwd": "",
+    "redis_port": 6379,
+
 }
 
 if SETTINGS['setting.env'] == 'dev':
